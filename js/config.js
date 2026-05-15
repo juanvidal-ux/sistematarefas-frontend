930c2d0fbc1df5daab6db76b727c1eed778b6663
juanvidal-ux <juan.vidal.claro@gmail.com>
2026-05-15 17:06:42 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-const API_URL =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-        ? "http://localhost:8081"
-        : "https://sistematarefas-backend.onrender.com";
